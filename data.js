69ef19e73ff9bc1a966820400a7f5c0fd7d9f3a5
Daniel Collins <danielmcollins1996@gmail.com>
2020-04-18 13:08:04 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+var csvJson = {
+    type: 'FeatureCollection',
+    features: [{
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: []
+        },
+        properties: {
+            title: '',
+            description: ''
+        }
+        }]
+}
---
+++ script.js
@@ -0,0 +1,43 @@
+var geojson = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.032, 38.913]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Washington, D.C.'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-122.414, 37.776]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'San Francisco, California'
+      }
+    }]
+  };
+console.log(map);
+console.log('hey');
+  geojson.features.forEach(function(marker) {
+
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+  
+    // make a marker for each feature and add to the map
+    new mapboxgl.Popup(el)
+      .setLngLat(marker.geometry.coordinates)
+      .setText(
+        'Construction on the Washington Monument began in 1848.'
+        )
+      .addTo(map);
+  })
+
+
